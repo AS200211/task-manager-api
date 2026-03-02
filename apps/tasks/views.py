8b0f404e174ddd6b732cfbfe4d3514b9b1baed6e
Akash Singhal <akash2002singhal@gmail.com>
2026-03-02 12:28:36 +0530

    Created migration for task table

--- apps/tasks/views.py
+++ apps/tasks/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import Task
+from .serializers import TaskSerializer
+from .permissions import IsOwnerOrAdmin
+
+
+class TaskViewSet(ModelViewSet):
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrAdmin]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.role == 'admin':
+            return Task.objects.all()
+        return Task.objects.filter(user=user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
